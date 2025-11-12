64ba567db87937c39b53f4bd93d818e06d00201e
g3m <75850489@continental.edu.pe>
2025-11-12 11:45:24 -0500

    Add sum calculation to main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ if __name__ == "__main__":
     nombre = input("¿Tu nombre? ")
     print(saludo(nombre))
     print("editar desde GitHub")
+    a = eval(input("Ingrese un numero: "))
+    b = eval(input("Ingrese un numero: "))
+    total = a + b
+    print("La suma de",a , "+", b, "es: ", total)
